c0642bf5c39994c74cb06d30f6a78efe3164494e
Aquarel <54329158+ThatAquarel@users.noreply.github.com>
2023-02-02 08:26:43 -0500

    Stats Rings

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,9 +1,43 @@
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  if (msg.color) {
-    console.log("Receive color = " + msg.color);
-    document.body.style.backgroundColor = msg.color;
-    sendResponse("Change color to " + msg.color);
-  } else {
-    sendResponse("Color message is none.");
-  }
-});
+import React from "react";
+import ReactDOM from "react-dom";
+import { StatsRing } from "./ui/stat_ring";
+
+function render_react() {
+  const tables = document.getElementsByClassName("BlueTableau");
+  if (!tables.length) return;
+
+  const react_root = document.createElement("div");
+  ReactDOM.render(
+    <div>
+      <StatsRing data={[
+          {
+            "label": "Page views",
+            "stats": "456,578",
+            "progress": 65,
+            "color": "teal",
+            "icon": "up"
+          },
+          {
+            "label": "New users",
+            "stats": "2,550",
+            "progress": 72,
+            "color": "blue",
+            "icon": "up"
+          },
+          {
+            "label": "Orders",
+            "stats": "4,735",
+            "progress": 52,
+            "color": "red",
+            "icon": "down"
+          }
+        ]
+      }/>
+    </div >,
+    react_root
+  );
+
+  tables[0].parentElement?.prepend(react_root);
+}
+
+render_react()
